e76472f4f136e5e6df385e45c2b6f8ba33477c75
Catherine <kankatya@bk.ru>
2022-08-22 20:40:56 +0500

    Added task21

---
+++ task21/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите координаты первой точки: ");
+int X1 = int.Parse(Console.ReadLine());
+int Y1 = int.Parse(Console.ReadLine());
+int Z1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты первой точки: ");
+int X2 = int.Parse(Console.ReadLine());
+int Y2 = int.Parse(Console.ReadLine());
+int Z2 = int.Parse(Console.ReadLine());
+double Distance = Math.Sqrt(Math.Pow(X2-X1, 2) +Math.Pow(Y2-Y1, 2) + Math.Pow(Z2 - Z1, 2));
+Console.WriteLine($"Расстояние между двумя точками: {Distance} .");
+
