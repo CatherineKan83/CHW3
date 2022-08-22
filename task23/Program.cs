942adefe60f57fc7f9bdbded06015452e1714860
Catherine <kankatya@bk.ru>
2022-08-23 00:08:11 +0500

    Added task23

---
+++ task23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Таблица кубов генерируется...");
+Console.WriteLine();
+for(int i=1; i<n+1; i++)
+{
+    Console.WriteLine($"{i} = {Math.Pow(i, 3)}");
+    Console.WriteLine();
+}
+Console.WriteLine("---");
